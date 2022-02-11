014b3dc503d993edfc72cc3426c64d91f0b4d20a
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2022-02-12 03:30:33 +0600

    create a new file category.blade.php and heading with paragaph

---
+++ resources/views/category.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Category Page</title>
+</head>
+<body>
+    <h1>This is a Category page </h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis 
+    aperiam aliquid facere sequi omnis corporis vitae error. Veritatis 
+    cumque voluptate cum ratione delectus aut, provident tenetur dolor 
+    tempore praesentium reprehenderit. cum ratione delectus aut, provident 
+    tenetur dolor tempore praesentium reprehenderit. cum ratione delectus aut, 
+    provident tenetur dolor tempore praesentium reprehenderit. cum ratione delectus 
+    aut, provident tenetur dolor tempore praesentium reprehenderit. cum ratione 
+    delectus aut, provident tenetur dolor tempore praesentium reprehenderit. cum 
+    ratione delectus aut, provident tenetur dolor tempore praesentium reprehenderit. 
+    cum ratione delectus aut, provident tenetur dolor tempore praesentium reprehenderit. 
+    cum ratione delectus aut, provident tenetur dolor tempore praesentium reprehenderit.
+</p>
+    
+</body>
+</html>
